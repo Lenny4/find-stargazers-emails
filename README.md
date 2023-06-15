cb36d35b5e76d1f70e275600af31525d3e74e01a
Alexandre Beaujour <alexandre.beaujour@hotmail.fr>
2023-06-15 08:56:05 -0400

    requests created

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Simple program to get all users email who star a repo
 git clone https://github.com/Lenny4/find-stargazers-emails.git
 docker compose up --build
 ```
+
+Go to https://github.com/settings/tokens/new to generate a token
+
+```bash
+echo TOKEN=> .env.local
+```
+Then place your token in it
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
 // https://khalilstemmler.com/blogs/typescript/node-starter-project/
 'use strict';
 
-console.log("todo");
+const maxRequestPerHour = 1000;
+const waitTime = (3601 / maxRequestPerHour) * 1000; // 1000 request per hour: rate limit https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#rate-limits-for-requests-from-github-actions
+const token = process.env.TOKEN;
+
+function getEmails(data: any, emails: string[]) {
+    for (const key in data) {
+        if (key === "email" && !emails.includes(data[key])) {
+            emails.push(data[key]);
+        }
+        if (data[key] !== null && typeof data[key] === 'object') {
+            getEmails(data[key], emails);
+        }
+    }
+}
+
+async function start() {
+    await new Promise(resolve => setTimeout(resolve, waitTime));
+    const stargazers = await (await fetch('https://api.github.com/repos/iperov/DeepFaceLab/stargazers', {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    })).json();
+    console.log(stargazers);
+    await new Promise(resolve => setTimeout(resolve, waitTime));
+    const user = await (await fetch('https://api.github.com/users/chakson', {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    })).json();
+    console.log(user);
+    await new Promise(resolve => setTimeout(resolve, waitTime));
+    const events = await (await fetch('https://api.github.com/users/lenny4/events/public', {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    })).json();
+    const emails: string[] = [];
+    getEmails(events, emails);
+    console.log(emails);
+}
+
+start();
